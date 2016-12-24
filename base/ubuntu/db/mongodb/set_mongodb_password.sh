2ba8412d7f3bbbba8fc860e1a46d2111f13f7d93
SharksevenRo <937257166@qq.com>
2016-12-24 11:33:37 +0800

    update

---
+++ base/ubuntu/db/mongodb/set_mongodb_password.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+USER=${DBUSER:-"admin"}
+DATABASE=${DBUSER:-"admin"}
+PASS=${DBPASS:-$(pwgen -s 12 1)}
+_word=$( [ ${DBPASS} ] && echo "preset" || echo "random" )
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for confirmation of MongoDB service startup"
+    sleep 5
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+
+echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+mongo admin --eval "db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'root',db:'admin'}]});"
+
+if [ "$DATABASE" != "admin" ]; then
+    echo "=> Creating an ${USER} user with a ${_word} password in MongoDB"
+    mongo admin -u $USER -p $PASS << EOF
+use $DATABASE
+db.createUser({user: '$USER', pwd: '$PASS', roles:[{role:'dbOwner',db:'$DATABASE'}]})
+EOF
+fi
+
+echo "=> Done!"
+touch /data/db/.DBPASSword_set
+
+echo "========================================================================"
+echo "You can now connect to this MongoDB server using:"
+echo ""
+echo "    mongo $DATABASE -u $USER -p $PASS --host <host> --port <port>"
+echo ""
+echo "Please remember to change the above password as soon as possible!"
+echo "========================================================================"
